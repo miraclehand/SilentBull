d55089ddf7f3909218e6f612d1c8ee05be3a2f4e
ypark <miraclehand@naver.com>
2025-05-26 15:03:57 +0900

    Fixed env files for android app

--- android-wearos/app/build.gradle.kts
+++ android-wearos/app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application")
-    kotlin("android")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
@@ -9,26 +9,38 @@ android {
 
     defaultConfig {
         applicationId = "com.silentbull"
-        minSdk = 30
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
-        wearableApp {
-            enabled = true
-        }
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
+    }
+    
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    
+    kotlinOptions {
+        jvmTarget = "1.8"
     }
 }
 
 dependencies {
-    implementation("androidx.wear:wear:1.2.0")
     implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.activity:activity-ktx:1.8.2")
+    
+    // Wear OS specific dependencies
+    implementation("androidx.wear:wear:1.3.0")
+    implementation("com.google.android.support:wearable:2.9.0")
+    compileOnly("com.google.android.wearable:wearable:2.9.0")
 }
-
--- android-wearos/build.gradle.kts
+++ android-wearos/build.gradle.kts
@@ -1,11 +1,8 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.3.0")
-        classpath(kotlin("gradle-plugin", version = "1.9.10"))
-    }
+plugins {
+    id("com.android.application") version "8.1.1" apply false
+    kotlin("android") version "1.9.10" apply false
 }
 
+tasks.register("clean", Delete::class) {
+    delete(layout.buildDirectory)
+}
