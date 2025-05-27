6a001677dac48baf39efcec90c2acde0dd0fd89f
ypark <miraclehand@naver.com>
2025-05-27 13:12:26 +0900

    Enhanced kotlin version

--- android-wearos/build.gradle.kts
+++ android-wearos/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application") version "8.1.1" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
-    kotlin("android") version "1.9.10" apply false
+    kotlin("android") version "1.9.23" apply false
 }
 
 tasks.register("clean", Delete::class) {
