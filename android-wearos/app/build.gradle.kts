1f8bff4401ca71c447700f1537d6dff8f6ee886f
ypark <miraclehand@naver.com>
2025-05-28 10:16:40 +0900

    Added signed configure

--- android-wearos/app/build.gradle.kts
+++ android-wearos/app/build.gradle.kts
@@ -16,6 +16,15 @@ android {
         versionName = "1.0"
     }
 
+    signingConfigs {
+        release {
+            storeFile file('../silentbull-release-key.keystore')
+            storePassword System.getenv('KEYSTORE_PASSWORD')
+            keyAlias System.getenv('KEY_ALIAS')
+            keyPassword System.getenv('KEY_PASSWORD')
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
